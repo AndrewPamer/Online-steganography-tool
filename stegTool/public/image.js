8b1ff8550283748f4ee4dd3f8e05d8fa837196d3
AndrewPamer <AndrewPamer@my.unt.edu>
2023-03-23 11:35:04 -0500

    Fixed #2. Changed Navigation Menu
    Fixed the bug where the image would take up too much space. Change the style of the navigation bar buttons.

--- stegTool/public/image.js
+++ stegTool/public/image.js
@@ -1,10 +1,15 @@
 function showImage() {
   const reader = new FileReader();
+  const reader2 =new FileReader();
   const imageShow = document.querySelector(".image");
   const image = document.querySelector("#inputImage").files[0];
   const b = document.querySelector(".image-preview");
   if (image) {
+    
     reader.readAsDataURL(image);
+    reader2.readAsBinaryString(image);
+    console.log(reader2);
+    
   }
 
   reader.addEventListener(
@@ -12,8 +17,8 @@ function showImage() {
     () => {
       imageShow.src = reader.result;
 
-      imageShow.style.border = "5px dotted white";
-      b.style.border = "";
+      // imageShow.style.border = "5px dotted white";
+      // b.style.border = "";
     },
     false
   );
