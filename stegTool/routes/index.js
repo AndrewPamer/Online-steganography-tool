886728709657ade131ef035ff482677058e6b37b
AndrewPamer <AndrewPamer@my.unt.edu>
2023-03-28 22:05:47 -0500

    Created Decoding page
    Created the decoding page jade file. Added implementation to the navigation menu buttons. Added decode route to index.js

--- stegTool/routes/index.js
+++ stegTool/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('encode', { title: 'Online Steganography Tool' });
+router.get("/", function (req, res, next) {
+  res.render("encode", { title: "Online Steganography Tool" });
+});
+
+router.get("/decode", function (req, res, next) {
+  res.render("decode", { title: "Online Steganography Tool" });
 });
 
 module.exports = router;
