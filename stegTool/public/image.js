e9eb13f7a3f0c3b45b637e5ca259e0c4d987539b
AndrewPamer <AndrewPamer@my.unt.edu>
2023-04-01 21:52:05 -0500

    Added Decode Page
    added decode page. The page will switch content when the user changes pages instead of actually going to a new page.

--- stegTool/public/image.js
+++ stegTool/public/image.js
@@ -1,8 +1,25 @@
-//Where the image preview will show
-const imagePreviewArea = document.querySelector(".image");
+//For when the user clicks one of the navigation menu buttons
+function navPage(showPage) {
+  let active = document.querySelector("#active");
+  let inactive = document.querySelector("#inactive");
+  if (showPage === "Encode") {
+    if (inactive.className === "encoding") {
+      active.id = "inactive";
+      inactive.id = "active";
+    }
+  } else {
+    if (inactive.className === "decoding") {
+      active.id = "inactive";
+      inactive.id = "active";
+    }
+  }
+}
 
 //Previews the inputted image
 function showImage(image) {
+  console.log(image);
+  //Where the image preview will show
+  const imagePreviewArea = document.querySelector("#active .image");
   const reader = new FileReader();
   if (image) {
     //Read the image as a file
@@ -16,15 +33,32 @@ function showImage(image) {
       imagePreviewArea.classList.remove("before");
       imagePreviewArea.classList.add("after");
       imagePreviewArea.src = reader.result;
+      // imageModifyTest(imagePreviewArea.src);
     },
     false
   );
 }
 
+//TEST FUNCTION
+/*
+function imageModifyTest(img) {
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
+
+  console.log(img.src);
+  let image = new Image();
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0);
+    var data = ctx.getImageData(0, 0, 500, 500);
+    console.log(data);
+  };
+  image.src = img;
+}
+*/
 //Called when the user inputs and image from the 'Input Image' Button
 function imageInputFromButton() {
   //Calls the function to show image
-  showImage(document.querySelector("#inputImage").files[0]);
+  showImage(document.querySelector("#active #inputImage").files[0]);
 }
 
 function dropImage(event) {
