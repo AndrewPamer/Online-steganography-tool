3a0ca36fbffe0fbea3d460b6aaa927a9ff2be440
AndrewPamer <AndrewPamer@my.unt.edu>
2023-04-04 19:10:34 -0500

    Added Encoding

--- stegTool/public/image.js
+++ stegTool/public/image.js
@@ -2,22 +2,25 @@
 function navPage(showPage) {
   let active = document.querySelector("#active");
   let inactive = document.querySelector("#inactive");
+  console.log(active, inactive);
   if (showPage === "Encode") {
-    if (inactive.className === "encoding") {
+    if (inactive.childNodes[0].className === "encoding") {
       active.id = "inactive";
       inactive.id = "active";
     }
   } else {
-    if (inactive.className === "decoding") {
+    if (inactive.childNodes[0].className === "decoding") {
       active.id = "inactive";
       inactive.id = "active";
     }
   }
 }
 
+var publicImage;
 //Previews the inputted image
 function showImage(image) {
-  console.log(image);
+  publicImage = image;
+  // console.log(image);
   //Where the image preview will show
   const imagePreviewArea = document.querySelector("#active .image");
   const reader = new FileReader();
@@ -33,10 +36,64 @@ function showImage(image) {
       imagePreviewArea.classList.remove("before");
       imagePreviewArea.classList.add("after");
       imagePreviewArea.src = reader.result;
-      // imageModifyTest(imagePreviewArea.src);
     },
     false
   );
+}
+
+
+function encodeImage() {
+  const dataReader = new FileReader();
+  if(publicImage) {
+    dataReader.readAsArrayBuffer(publicImage);
+  }
+
+  dataReader.addEventListener("load", () => {
+    //Get the text input
+    const text = document.querySelector("#active textarea").value
+    //Text encoder for the inputted text
+    const encoder = new TextEncoder();
+    //Encode the text into Uint8Array
+    const encodedText = encoder.encode(text);
+
+    //Combine the Uint8Arrays of the image and the text
+    var combinedBuffers = new Uint8Array([...(new Uint8Array(dataReader.result)), ...encodedText]);
+    //Create URL image from the buffer
+    const encodedImageURL = URL.createObjectURL(new Blob([combinedBuffers], {type: 'image/jpg'}));
+
+    //Create a new image object and set the source to the image
+    const encodeContainer = document.querySelector(".encoded-container");
+    const encodeHeader = encodeContainer.appendChild(document.createElement("h2"));
+    encodeHeader.textContent = "Encoded Image"
+
+    const imgObj = encodeContainer.appendChild(document.createElement("img"))
+    imgObj.classList.add("encodedImage")
+    imgObj.src = encodedImageURL;
+
+    const downloadButton = encodeContainer.appendChild(document.createElement("button"));
+    downloadButton.classList.add("button");
+    downloadButton.id = "download"
+    downloadButton.textContent="Download";
+    downloadButton.setAttribute("onClick", "downloadImage()")
+    
+
+    imgObj.addEventListener("load", () => {
+      imgObj.scrollIntoView({ behavior: "smooth", block: "end"});
+      //fadein
+    })
+    
+}, false);
+
+}
+
+function downloadImage() {
+  // const downloadButton = document.querySelector("#active #download")
+  // downloadButton.addEventListener
+  const downloadLink = document.createElement("a");
+  downloadLink.href = document.querySelector("#active .encoded-container img").src
+  downloadLink.download = "encodedImage.jpg";
+  downloadLink.click();
+  console.log(downloadLink);
 }
 
 //TEST FUNCTION
@@ -64,10 +121,8 @@ function imageInputFromButton() {
 function dropImage(event) {
   event.preventDefault();
   if (event.dataTransfer.items) {
-    console.log(event.dataTransfer.items);
     [...event.dataTransfer.items].forEach((item) => {
       if (item.kind == "file" && item.type.match(/image/)) {
-        console.log(item);
         showImage(item.getAsFile());
       }
     });
@@ -75,6 +130,5 @@ function dropImage(event) {
 }
 
 function dragImageOver(event) {
-  console.log("file in area");
   event.preventDefault();
 }
