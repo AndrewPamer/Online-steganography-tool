742959202d2d7874b60cf0177ad89e626cbf1c26
AndrewPamer <AndrewPamer@my.unt.edu>
2023-04-05 23:40:37 -0500

    Added Decoding functionality for padding method
    Added decoding functionality for the padding encoding method. Only works for jpg/jpeg images currently.

--- stegTool/public/image.js
+++ stegTool/public/image.js
@@ -23,6 +23,7 @@ function showImage(image) {
   // console.log(image);
   //Where the image preview will show
   const imagePreviewArea = document.querySelector("#active .image");
+  console.log(imagePreviewArea);
   const reader = new FileReader();
   if (image) {
     //Read the image as a file
@@ -41,77 +42,96 @@ function showImage(image) {
   );
 }
 
-
 function encodeImage() {
   const dataReader = new FileReader();
-  if(publicImage) {
+  if (publicImage) {
     dataReader.readAsArrayBuffer(publicImage);
   }
 
-  dataReader.addEventListener("load", () => {
-    //Get the text input
-    const text = document.querySelector("#active textarea").value
-    //Text encoder for the inputted text
-    const encoder = new TextEncoder();
-    //Encode the text into Uint8Array
-    const encodedText = encoder.encode(text);
+  dataReader.addEventListener(
+    "load",
+    () => {
+      //Get the text input
+      const text = document.querySelector("#active textarea").value;
+      //Text encoder for the inputted text
+      const encoder = new TextEncoder();
+      //Encode the text into Uint8Array
+      const encodedText = encoder.encode(text);
 
-    //Combine the Uint8Arrays of the image and the text
-    var combinedBuffers = new Uint8Array([...(new Uint8Array(dataReader.result)), ...encodedText]);
-    //Create URL image from the buffer
-    const encodedImageURL = URL.createObjectURL(new Blob([combinedBuffers], {type: 'image/jpg'}));
+      //Combine the Uint8Arrays of the image and the text
+      var combinedBuffers = new Uint8Array([
+        ...new Uint8Array(dataReader.result),
+        ...encodedText,
+      ]);
+      //Create URL image from the buffer
+      const encodedImageURL = URL.createObjectURL(
+        new Blob([combinedBuffers], { type: "image/jpg" })
+      );
 
-    //Create a new image object and set the source to the image
-    const encodeContainer = document.querySelector(".encoded-container");
-    const encodeHeader = encodeContainer.appendChild(document.createElement("h2"));
-    encodeHeader.textContent = "Encoded Image"
+      //Create a new image object and set the source to the image
+      const encodeContainer = document.querySelector(".encoded-container");
+      const encodeHeader = encodeContainer.appendChild(
+        document.createElement("h2")
+      );
+      encodeHeader.textContent = "Encoded Image";
 
-    const imgObj = encodeContainer.appendChild(document.createElement("img"))
-    imgObj.classList.add("encodedImage")
-    imgObj.src = encodedImageURL;
+      const imgObj = encodeContainer.appendChild(document.createElement("img"));
+      imgObj.classList.add("encodedImage");
+      imgObj.src = encodedImageURL;
 
-    const downloadButton = encodeContainer.appendChild(document.createElement("button"));
-    downloadButton.classList.add("button");
-    downloadButton.id = "download"
-    downloadButton.textContent="Download";
-    downloadButton.setAttribute("onClick", "downloadImage()")
-    
+      const downloadButton = encodeContainer.appendChild(
+        document.createElement("button")
+      );
+      downloadButton.classList.add("button");
+      downloadButton.id = "download";
+      downloadButton.textContent = "Download";
+      downloadButton.setAttribute("onClick", "downloadImage()");
 
-    imgObj.addEventListener("load", () => {
-      imgObj.scrollIntoView({ behavior: "smooth", block: "end"});
-      //fadein
-    })
-    
-}, false);
+      imgObj.addEventListener("load", () => {
+        imgObj.scrollIntoView({ behavior: "smooth", block: "end" });
+      });
+    },
+    false
+  );
+}
 
+function decodeImage() {
+  const imageToDecode = document.querySelector("#active #inputImage").files[0];
+  const decodeTextArea = document.querySelector("#active textarea");
+  const decodeReader = new FileReader();
+  if (imageToDecode) {
+    decodeReader.readAsArrayBuffer(imageToDecode);
+  }
+
+  decodeReader.addEventListener("load", () => {
+    const data = new Uint8Array(decodeReader.result);
+    //Get to the FF D8 at the end of a jpg header
+    const lastIndex = data.lastIndexOf(217);
+    if (lastIndex != -1) {
+      const text = new TextDecoder().decode(data.subarray(lastIndex + 1));
+      if (text === "") {
+        alert("Could not find any encoded data!");
+      }
+      decodeTextArea.value = text;
+    } else {
+      alert("Error with decoding the image");
+      return;
+    }
+  });
 }
 
 function downloadImage() {
   // const downloadButton = document.querySelector("#active #download")
   // downloadButton.addEventListener
   const downloadLink = document.createElement("a");
-  downloadLink.href = document.querySelector("#active .encoded-container img").src
+  downloadLink.href = document.querySelector(
+    "#active .encoded-container img"
+  ).src;
   downloadLink.download = "encodedImage.jpg";
   downloadLink.click();
   console.log(downloadLink);
 }
 
-//TEST FUNCTION
-/*
-function imageModifyTest(img) {
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
-
-  console.log(img.src);
-  let image = new Image();
-  image.onload = () => {
-    ctx.drawImage(image, 0, 0);
-    var data = ctx.getImageData(0, 0, 500, 500);
-    console.log(data);
-  };
-  image.src = img;
-}
-*/
 //Called when the user inputs and image from the 'Input Image' Button
 function imageInputFromButton() {
   //Calls the function to show image
