78b929a18901a5873201afae84019530aa0201da
Nick Wylie <98427775+NickHubGitHub@users.noreply.github.com>
2023-04-23 10:39:19 -0500

    instruction popups for the decode page

--- stegTool/public/instructions.js
+++ stegTool/public/instructions.js
@@ -1,33 +1,50 @@
-// Select the container element for the information popups
 const infoContainer = document.querySelector('.info-container');
 
-// Define the instructions to show in the popups
-const instructions = [
+const encodeInstructions = [
   'To encode an image, select the image which you wish to encode. You can click the box below to upload an image from your device, or you can drag and drop an image into the box to upload it.',
   'Input the text you wish to encode into the image below. For LSB method, the size of the text to encoded will be limited by the size of the uploaded image. File padding method is limited to 30 characters. ',
-  'Choose which encoding method you want to perform.'
+  'Choose which encoding method you want to perform.',
+  'Press the encode image button to download your new encyrpted image.'
 ];
 
-// Create an array of CSS classes for each instruction
-const cssClasses = [
+const encodeCssClasses = [
   'instruction-1',
   'instruction-2',
-  'instruction-3'
+  'instruction-3',
+  'instruction-4'
+];
+
+const decodeInstructions = [
+  'To decode an image, select the image which you wish to decode. You can click the box below to upload an image from your device, or you can drag and drop an image into the box to upload it.',
+  'Once you press the Decode button, any encoded text will be displayed along with the metadata and bitplanes of the image.'
+];
+
+const decodeCssClasses = [
+  'dinstruction-1',
+  'dinstruction-2'
 ];
 
 let instructionIndex = 0;
 let popupsShown = false;
 
-// Add an event listener to the container element for clicks
 infoContainer.addEventListener('click', function() {
-  // Check if the popups have already been shown and if the encode button is active
-  if (!popupsShown && document.querySelector("#activeButton").innerText === "Encode") {
+  if (!popupsShown) {
     popupsShown = true;
     const popup = document.createElement('div');
     popup.classList.add('popup');
+
+    let instructions, cssClasses;
+
+    if (document.querySelector("#activeButton").innerText === "Encode") {
+      instructions = encodeInstructions;
+      cssClasses = encodeCssClasses;
+    } else {
+      instructions = decodeInstructions;
+      cssClasses = decodeCssClasses;
+    }
+
     popup.classList.add(cssClasses[instructionIndex]);
 
-    // Create a heading for the popup and add the instruction text
     const heading = document.createElement('h2');
     heading.innerText = 'Step ' + (instructionIndex + 1);
 
@@ -35,12 +52,10 @@ infoContainer.addEventListener('click', function() {
     instruction.classList.add('instruction');
     instruction.innerText = instructions[instructionIndex];
 
-    // Create a button to go to the next instruction
     const nextButton = document.createElement('button');
     nextButton.classList.add('next-button');
     nextButton.innerText = 'Next';
 
-    // Add an event listener to the button to show the next instruction
     nextButton.addEventListener('click', function() {
       instructionIndex++;
       if (instructionIndex < instructions.length) {
@@ -50,15 +65,33 @@ infoContainer.addEventListener('click', function() {
         popup.classList.add(cssClasses[instructionIndex]);
       } else {
         document.body.removeChild(popup);
+        instructionIndex = 0; 
+      }
+      
+      if (instructionIndex >= instructions.length) {
+        instructionIndex = 0;
       }
     });
 
-    // Add the heading, instruction, and button to the popup
     popup.appendChild(heading);
     popup.appendChild(instruction);
     popup.appendChild(nextButton);
 
-    // Add the popup to the document body
     document.body.appendChild(popup);
   }
+});
+
+const encodeButton = document.querySelector('#encodeButton');
+const decodeButton = document.querySelector('#decodeButton');
+
+encodeButton.addEventListener('click', function() {
+  popupsShown = false;
+});
+
+decodeButton.addEventListener('click', function() {
+  popupsShown = false;
+});
+
+document.addEventListener('click', function() {
+  popupsShown = false;
 });
